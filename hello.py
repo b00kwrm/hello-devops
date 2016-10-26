d048c8ca93c61b45cdb01dd09b87b3160e8da940
b00kwrm <david.heatonian@gmail.com>
2016-10-26 19:59:50 -0400

    Added circle.yml

--- hello.py
+++ hello.py
@@ -1,9 +1,21 @@
 from flask import Flask
 app = Flask(__name__)
 
+page = b"""
+<!doctype html>
+<html>
+<head>
+<title>hello world</title>
+</head>
+<body>
+<h1>hello, world</h1>
+</body>
+</html>
+"""
+
 @app.route("/")
 def hello():
-    return "Hello World"
+    return page
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
