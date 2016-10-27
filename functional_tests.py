74e7d5e3ec3e90ef93fb067685ea18961159d036
b00kwrm <david.heatonian@gmail.com>
2016-10-26 20:01:04 -0400

    added functional test and circle file

---
+++ functional_tests.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+import unittest
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_get_hello_world(self):
+        # Edith goes to the homepage.  
+        self.browser.get('http://localhost:5000')
+
+        # She notices the title page mentions hello world.
+        self.assertIn('hello world', self.browser.title)
+        #self.fail('Finish the test!')
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
